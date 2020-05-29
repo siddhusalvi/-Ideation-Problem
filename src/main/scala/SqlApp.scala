a01231be1686d4149805eaa6314ad0645d4d6cbc
siddhusalvi <siddheshsalvi525@gmail.com>
2020-05-30 03:30:47 +0530

    TESTING : Timestamp data

--- src/main/scala/SqlApp.scala
+++ src/main/scala/SqlApp.scala
@@ -1,6 +1,6 @@
 import SqlApp.logDfSchema
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.types.{DateType, DoubleType, IntegerType, StringType, StructField, StructType, TimestampType}
 import org.apache.spark.sql.functions._
 import org.joda.time.DateTimeFieldType
 object SqlApp extends App{
@@ -11,7 +11,7 @@ object SqlApp extends App{
 
   val logDfSchema = StructType(
     Array(
-      StructField("DateTime",StringType),
+      StructField("DateTime",TimestampType),
       StructField("Cpu_Count",IntegerType),
       StructField("Cpu_Working_Time",DoubleType),
       StructField("Cpu_Idle_Time",DoubleType),
@@ -63,34 +63,50 @@ object SqlApp extends App{
   val postPath=").csv"
 
   val logDF1 = getDfFromCsv(prePath+"1"+postPath,spark,logDfSchema)
-  val logDF2 = getDfFromCsv(prePath+"2"+postPath,spark,logDfSchema)
-  val logDF3 = getDfFromCsv(prePath+"3"+postPath,spark,logDfSchema)
-  val logDF4 = getDfFromCsv(prePath+"4"+postPath,spark,logDfSchema)
-  val logDF5 = getDfFromCsv(prePath+"5"+postPath,spark,logDfSchema)
-  val logDF6 = getDfFromCsv(prePath+"6"+postPath,spark,logDfSchema)
-  val logDF7 = getDfFromCsv(prePath+"7"+postPath,spark,logDfSchema)
-  val logDF8 = getDfFromCsv(prePath+"8"+postPath,spark,logDfSchema)
-  val logDF9 = getDfFromCsv(prePath+"9"+postPath,spark,logDfSchema)
 
-  val logDF = logDF1.union(logDF2).union(logDF3).union(logDF4).union(logDF5).union(logDF6).union(logDF7).union(logDF8).union(logDF9)
-  import spark.implicits._
-
-    val idleUserDetails = logDF.select(
-      logDF.col("User_Name"),
-      logDF.col("Cpu_Idle_Time")
-    )
-
-
-  val highestIdleUser = idleUserDetails.groupBy(col("User_Name"))
-  .sum("Cpu_Idle_Time").withColumnRenamed("sum(Cpu_Idle_Time)","Cpu_Idle_Time")
-    .orderBy(col("Cpu_Idle_Time").desc)
-    .withColumnRenamed("Cpu_Idle_Time","highest_Idle_hours")
-    .show(1)
+  logDF1.show()
+  //  val logDF2 = getDfFromCsv(prePath+"2"+postPath,spark,logDfSchema)
+//  val logDF3 = getDfFromCsv(prePath+"3"+postPath,spark,logDfSchema)
+//  val logDF4 = getDfFromCsv(prePath+"4"+postPath,spark,logDfSchema)
+//  val logDF5 = getDfFromCsv(prePath+"5"+postPath,spark,logDfSchema)
+//  val logDF6 = getDfFromCsv(prePath+"6"+postPath,spark,logDfSchema)
+//  val logDF7 = getDfFromCsv(prePath+"7"+postPath,spark,logDfSchema)
+//  val logDF8 = getDfFromCsv(prePath+"8"+postPath,spark,logDfSchema)
+//  val logDF9 = getDfFromCsv(prePath+"9"+postPath,spark,logDfSchema)
+//
+//  val logDF = logDF1.union(logDF2).union(logDF3).union(logDF4).union(logDF5).union(logDF6).union(logDF7).union(logDF8).union(logDF9)
+//  import spark.implicits._
+//
+//  //Finding highest idle user
+//    val idleUserDetails = logDF.select(
+//      logDF.col("User_Name"),
+//      logDF.col("Cpu_Idle_Time")
+//    )
+//
+//  val highestIdleUser = idleUserDetails.groupBy(col("User_Name"))
+//  .sum("Cpu_Idle_Time").withColumnRenamed("sum(Cpu_Idle_Time)","Cpu_Idle_Time")
+//    .orderBy(col("Cpu_Idle_Time").desc)
+//    .withColumnRenamed("Cpu_Idle_Time","highest_Idle_hours")
+//    .show(1)
+//
+//  //Finding highest working user
+//  val workingUserDetails = logDF.select(
+//    logDF.col("User_Name"),
+//    logDF.col("Keyboard") + logDF.col("Mouse") as("Working_Action")
+//  )
+//
+//
+//  val highestWorkingUser = workingUserDetails.groupBy(col("User_Name"))
+//    .sum("Working_Action").withColumnRenamed("sum(Working_Action)","Working_Action")
+//    .orderBy(col("Working_Action").desc)
+//    .withColumnRenamed("Working_Action","highest_Working_User")
+//    .show(1)
 
   def getDfFromCsv(path:String,spark:SparkSession,strct:StructType): DataFrame ={
     val DF = spark.read
       .format("csv")
       .option("header", true)
+      .option("timestampFormat" , "MM/dd/yyyy HH:mm:ss.SSSSSS")
       .schema(strct)
       .load(path)
     DF
